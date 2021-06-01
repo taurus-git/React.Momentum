a183c039dfdda0a55c4705eb6d6e0ca2dbf20f73
taurus-git <t.1113@ya.ru>
2021-06-01 13:44:20 +0300

    Changed proxy path

--- src/Weather/apis/DailyForecast.js
+++ src/Weather/apis/DailyForecast.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let DailyForecast = async (city) => {
-    const corsProxyServer = 'https://thingproxy.freeboard.io/fetch/';
+    const corsProxyServer = 'https://cors-anywhere.herokuapp.com/';
 
     let weatherData = await axios.get(
         `${corsProxyServer}https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=b0e3cfcac6b3bb698bc3351ad3e21975`
